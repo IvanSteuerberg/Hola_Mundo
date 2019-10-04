59d18cbc9ffe1b0f0f1aa7ac3174256093d91e94
Ivan Steuerberg <ivansteuerberg@danielcastelao.org>
2019-10-04 14:06:43 +0200

    Inicio

---
+++ holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * La clase HolaMundo saca Hola Mundo por pantalla.
+ */
+package holamundo;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hola Mundo!"); // Display the string.
+    }
+    
+}
